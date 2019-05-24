205d533d55f3c01d1fe0b06342259348e703ec13
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-24 21:33:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.join(" and ")
   else array.count == 3 || array.count > 3
     #array.join(", ").insert(-2, "and")
-    array.insert(-2, "and").join(", ").split(", ")
+    array.insert(-2, "and").join(", ")
     #array.join(", ")<< " and"
   end
 end
